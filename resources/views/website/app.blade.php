4993d36320ee900ef3d3265cf5d4ecd5b8d294e5
danieluyo <dsalcedo@outlook.com>
2016-09-07 00:43:08 -0500

    #fix website components

--- resources/views/website/app.blade.php
+++ resources/views/website/app.blade.php
@@ -6,14 +6,23 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Index</title>
     <link href="{{asset('/css/bootstrap.min.css')}}" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
     @yield('css')
 </head>
 <body>
+
+@include('website.partials.navbar')
 
 @yield('body')
 
 <script src="{{asset('/js/jquery.min.js')}}"></script>
 <script src="{{asset('/js/bootstrap.min.js')}}"></script>
 @yield('js')
+
 </body>
 </html>
